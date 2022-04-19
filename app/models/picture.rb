24786c33842b003211fc8df24831e7f2d87f2645
junho jang <cclemon3248@gmail.com>
2022-04-19 09:10:39 +0900

    日本語の辞書ファイル

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,6 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :title, presence: true
+  validates :image, presence: true
 end
